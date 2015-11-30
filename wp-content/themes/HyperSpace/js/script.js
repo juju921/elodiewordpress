4dd27943f11d640028d85a88c50f2b9eaba8af51
juju921 <julien.garretb@gmail.com>
2015-11-30 16:53:59 +0100

    changement masonry

--- wp-content/themes/HyperSpace/js/script.js
+++ wp-content/themes/HyperSpace/js/script.js
@@ -1,6 +1,18 @@
 jQuery(document).ready( function($) {
     var $container = $('.grid');
-    $container.masonry({
+    jQuery('.grid').masonry({
+        itemSelector : '.grid-item',
+        columnWidth: 290,
+        gutter: 10,
+        isAnimated: true,
+        isFitWidth: true,
+        containerStyle: { position: 'relative' },
+        isResizable: true
+    });
+
+
+
+   /* $container.masonry({
         // options
         itemSelector: '.grid-item',
         columnWidth: 290,
@@ -10,11 +22,19 @@ jQuery(document).ready( function($) {
     }).imagesLoaded(function(){
         $container.masonry('reloadItems');
         $container.masonry('layout');
-    });
+    });*/
 
     if($(window).width() <= 400){
         $container.masonry( 'destroy' );
         console.log('toto');
     }
+
+    /*jQuery('#simplemasonry-container-66').masonry({
+        itemSelector : '.simplemasonry-item-66',
+        isAnimated: true,
+        isFitWidth: true,
+        containerStyle: { position: 'relative' },
+        isResizable: true
+    });*/
 
 } );
